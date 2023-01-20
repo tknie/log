a2908dede155bfa32afa8bb6caf2e0c0d54fc331
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2023-01-20 15:12:41 +0100

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# General framework for log
+
+This is a small project but code I used in each project.
+I'd like to be independent to a logging framework. That's why I use this to wrap it.
+
+Used log frameworks in the past
+
+* logrus (github.com/sirupsen/logrus)
+* zap logging (go.uber.org/zap )
+* own logging framework
---
+++ logging_test.go
@@ -0,0 +1,154 @@
+package log
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func initTestLogWithFile(t *testing.T, fileName string) error {
+	err := initLogLevelWithFile(fileName, zapcore.DebugLevel)
+	if err != nil {
+		t.Fatalf("error opening file: %v", err)
+	}
+	return err
+}
+
+func newWinFileSink(u *url.URL) (zap.Sink, error) {
+	// Remove leading slash left by url.Parse()
+	return os.OpenFile(u.Path[1:], os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+}
+
+func initLogLevelWithFile(fileName string, level zapcore.Level) (err error) {
+	p := os.Getenv("LOGPATH")
+	if p == "" {
+		p = os.TempDir()
+	}
+	var name string
+	if runtime.GOOS == "windows" {
+		zap.RegisterSink("winfile", newWinFileSink)
+		//		OutputPaths: []string{"stdout", "winfile:///" + filepath.Join(GlobalConfigDir.Path, "info.log.json")},
+		name = "winfile:///" + p + string(os.PathSeparator) + fileName
+	} else {
+		name = "file://" + filepath.ToSlash(p+string(os.PathSeparator)+fileName)
+	}
+
+	rawJSON := []byte(`{
+	"level": "error",
+	"encoding": "console",
+	"outputPaths": [ "XXX"],
+	"errorOutputPaths": ["stderr"],
+	"encoderConfig": {
+	  "messageKey": "message",
+	  "levelKey": "level",
+	  "levelEncoder": "lowercase"
+	}
+  }`)
+
+	var cfg zap.Config
+	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+		return err
+	}
+	cfg.Level.SetLevel(level)
+	cfg.OutputPaths = []string{name}
+	logger, err := cfg.Build()
+	if err != nil {
+		return err
+	}
+	defer logger.Sync()
+
+	sugar := logger.Sugar()
+	Log = sugar
+
+	sugar.Infof("AdabasGoApi logger initialization succeeded")
+	return nil
+}
+
+func doTrackCall() {
+	defer TimeTrack(time.Now(), "Time Track Unit test ")
+
+}
+
+func TestLogZap(t *testing.T) {
+	fileName := "zap.log"
+	os.Remove(os.TempDir() + "/" + fileName)
+	err := initTestLogWithFile(t, fileName)
+	if !assert.NoError(t, err) {
+		fmt.Println(err)
+		return
+	}
+
+	d := IsDebugLevel()
+	SetDebugLevel(true)
+	doTrackCall()
+
+	hallo := "HELLO"
+	Log.Debugf("This is a test of data %s", hallo)
+
+	LogMultiLineString(true, "ABC\nXXXX\n")
+	SetDebugLevel(false)
+	SetDebugLevel(d)
+
+	flog, err := os.Open(os.TempDir() + "/" + fileName)
+	if !assert.NoError(t, err) {
+		return
+	}
+	logInfo, err := io.ReadAll(flog)
+	if !assert.NoError(t, err) {
+		return
+	}
+	assert.Regexp(t, `info	AdabasGoApi logger initialization succeeded
+info	Time Track Unit test  took \d*ns
+debug	This is a test of data HELLO
+debug	ABC
+debug	XXXX
+debug`, string(logInfo))
+}
+
+func TestLogrus(t *testing.T) {
+	fileName := "logrus.log"
+	os.Remove(os.TempDir() + "/" + fileName)
+	log := logrus.StandardLogger()
+
+	fmt.Println("Init logging")
+	SetDebugLevel(true)
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02T15:04:05",
+	})
+	log.SetLevel(logrus.DebugLevel)
+	p := os.Getenv("LOGPATH")
+	if p == "" {
+		p = os.TempDir()
+	}
+	f, err := os.OpenFile(p+"/"+fileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	if !assert.NoError(t, err) {
+		fmt.Println("Error opening log:", err)
+		return
+	}
+	log.SetOutput(f)
+	log.Infof("Init logrus")
+	Log = log
+	fmt.Println("Logging running")
+
+	flog, err := os.Open(os.TempDir() + "/" + fileName)
+	if !assert.NoError(t, err) {
+		return
+	}
+	logInfo, err := io.ReadAll(flog)
+	if !assert.NoError(t, err) {
+		return
+	}
+	assert.Regexp(t, "time=\"20..-..-..T..:..:..\" level=info msg=\"Init logrus\"\n", string(logInfo))
+}
