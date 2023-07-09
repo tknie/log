620bb99e7bd69982ff7ef2ad38ad0efb07f929fa
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2023-07-09 14:33:01 +0200

    Add Fatalf

--- logging.go
+++ logging.go
@@ -1,5 +1,5 @@
 /*
-* Copyright 2022 Thorsten A. Knieling
+* Copyright 2022-2023 Thorsten A. Knieling
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -34,6 +34,9 @@ func (*nilLogger) Errorf(format string, args ...interface{}) {
 }
 
 func (*nilLogger) Fatal(args ...interface{}) {
+}
+
+func (*nilLogger) Fatalf(format string, args ...interface{}) {
 }
 
 // Log defines the log interface to manage other Log output frameworks
